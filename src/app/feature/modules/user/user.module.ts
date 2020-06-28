6ff4a7dce2f3c14769b1551f6d68bf2fcdc1204b
Tasmiya Fatema <tasmiya.shah36@gmail.com>
2020-06-28 13:59:34 +0545

    Feature: Added UserFormComponent modal.

--- src/app/feature/modules/user/user.module.ts
+++ src/app/feature/modules/user/user.module.ts
@@ -4,14 +4,22 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './components/user/user.component';
 import { ThemeModule } from 'src/app/@theme/theme.module';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { NbDialogModule } from '@nebular/theme';
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UserFormComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
-    ThemeModule
-  ]
+    ThemeModule,
+    NbDialogModule.forChild({
+      closeOnEsc: true,
+      hasScroll: true,
+      closeOnBackdropClick: false,
+    }),
+  ],
+  entryComponents: [UserFormComponent]
 })
 export class UserModule { }
