18fbb1c0b60e5261da438628411e124b4ea895e9
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-06-30 13:12:04 +0545

    manage department

--- src/app/@core/constants/app/app.constant.ts
+++ src/app/@core/constants/app/app.constant.ts
@@ -2,4 +2,5 @@ export class AppConstant {
     static YES = 'Yes';
     static NO = 'No';
     static USER_DELETE_CONFIRMATION = 'Are you sure you want to delete the user?';
+    static DEPARTMENT_DELETE_CONFIRMATION = 'Are you sure you want to delete the department?';
 }
