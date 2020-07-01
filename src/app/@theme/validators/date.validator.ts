ca0cb151d74efbd40b8970d449aad27171ebd23f
Elvin Shrestha <elwyncrestha@gmail.com>
2020-07-01 09:51:08 +0545

    Refactor: Refactor DateValidator

--- src/app/@theme/validators/date.validator.ts
+++ src/app/@theme/validators/date.validator.ts
@@ -5,7 +5,7 @@ export class DateValidator {
      * Validate if the date is past date or not.
      * @param control An AbstractControl or other specialized controls.
      */
-    static pastDate(control: AbstractControl): { [key: string]: boolean | null {
+    static pastDate(control: AbstractControl): { [key: string]: boolean } | null {
         const value = control.value;
         const date = new Date(value);
         if (date > new Date()) {
@@ -18,7 +18,7 @@ export class DateValidator {
      * Validate if the date if future date or not.
      * @param control An AbstractControl or other specialized controls.
      */
-    static futureDate(control: AbstractControl): { [key: string]: boolean | null {
+    static futureDate(control: AbstractControl): { [key: string]: boolean } | null {
         const value = control.value;
         const date = new Date(value);
         if (date < new Date()) {
