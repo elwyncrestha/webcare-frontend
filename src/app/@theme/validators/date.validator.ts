c4791f3d38158a5cd5691df149af670ce13399b5
Prerana Pandit <preranapandit07@gmail.com>
2020-07-01 09:55:23 +0545

    Refactor: Replaced custom validator return type with ValidationErrors.

--- src/app/@theme/validators/date.validator.ts
+++ src/app/@theme/validators/date.validator.ts
@@ -1,11 +1,11 @@
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class DateValidator {
     /**
      * Validate if the date is past date or not.
      * @param control An AbstractControl or other specialized controls.
      */
-    static pastDate(control: AbstractControl): { [key: string]: boolean } | null {
+    static pastDate(control: AbstractControl): ValidationErrors | null {
         const value = control.value;
         const date = new Date(value);
         if (date > new Date()) {
@@ -18,7 +18,7 @@ export class DateValidator {
      * Validate if the date if future date or not.
      * @param control An AbstractControl or other specialized controls.
      */
-    static futureDate(control: AbstractControl): { [key: string]: boolean } | null {
+    static futureDate(control: AbstractControl): ValidationErrors | null {
         const value = control.value;
         const date = new Date(value);
         if (date < new Date()) {
